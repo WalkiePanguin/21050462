12e3fd3121b7319d83873d26119ab22d8ba2707a
WalkiePanguin <nootmin@hotmail.com>
2022-02-24 00:24:39 +0800

    xd

--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,1 @@
-docker container run --name stage2-21050462-container -d -p 5000:80 -it apache2-21050462-image
+sudo docker container run --name stage2-21050462-container -d -p 5000:80 -it apache2-21050462-image
